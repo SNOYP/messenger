f3c727ebeb2e7fee311392f9ace44104175707f4
SNOYP <145098118+SNOYP@users.noreply.github.com>
2026-05-26 19:59:42 +0300

    Delete Massenger/messenger-server directory

--- Massenger/messenger-server/src/main/java/com/example/messengerserver/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.messengerserver;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Spring сам напишет за нас все методы вроде save(), findAll(), delete()
-}
